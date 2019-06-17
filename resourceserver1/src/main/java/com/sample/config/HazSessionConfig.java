348c891232b1a5cf905d2696677e8630cf6dfcb4
dhilish <51811472+dhilish@users.noreply.github.com>
2019-06-17 16:46:51 +0530

    Added new files

---
+++ resourceserver1/src/main/java/com/sample/config/HazSessionConfig.java
@@ -0,0 +1,143 @@
+package com.sample.config;
+
+import java.io.FileNotFoundException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.session.hazelcast.HazelcastSessionRepository;
+import org.springframework.session.hazelcast.PrincipalNameExtractor;
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.MapAttributeConfig;
+import com.hazelcast.config.MapIndexConfig;
+import com.hazelcast.config.SerializerConfig;
+import com.hazelcast.config.XmlConfigBuilder;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.sample.web.initializer.ObjectStreamSerializer;
+
+
+@Profile("!dev")
+@EnableHazelcastHttpSession
+public class HazSessionConfig {
+
+	@Value("${spring.profiles.active}")
+	private String profile;
+	
+	@Value("${hazelcast.config.folder}")
+	private String hazelcastFolder;
+	
+	@Profile("!dev")
+	@Bean
+	public HazelcastInstance hazelcastSession() throws FileNotFoundException {
+		try {
+			MapAttributeConfig attributeConfig = new MapAttributeConfig()
+					.setName(HazelcastSessionRepository.PRINCIPAL_NAME_ATTRIBUTE)
+					.setExtractor(PrincipalNameExtractor.class.getName());
+			
+		Config config= new XmlConfigBuilder(hazelcastFolder.concat(profile).concat("_HazelcastDistributedSession.xml")).build();
+		
+		SerializerConfig serializer = new SerializerConfig()
+				.setImplementation(new ObjectStreamSerializer())
+				.setTypeClass(Object.class);
+
+		config.getSerializationConfig()
+				.addSerializerConfig(serializer);
+		
+		config.getMapConfig(HazelcastSessionRepository.DEFAULT_SESSION_MAP_NAME) 
+		.addMapAttributeConfig(attributeConfig)
+		.addMapIndexConfig(new MapIndexConfig(
+				HazelcastSessionRepository.PRINCIPAL_NAME_ATTRIBUTE, false));
+		return  Hazelcast.newHazelcastInstance(config); 
+		}
+		catch(FileNotFoundException e) {
+			throw new FileNotFoundException("Hazelcast Configuration with ".concat(hazelcastFolder.concat(profile).concat("_HazelcastDistributedSession.xml")).concat(" Not Found"));
+		}
+		
+
+	}
+	
+	
+}
+
+/*
+ * Copyright 2014-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*package com.sample.config;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.MapAttributeConfig;
+import com.hazelcast.config.MapIndexConfig;
+import com.hazelcast.config.SerializerConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.sample.web.initializer.ObjectStreamSerializer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.hazelcast.HazelcastSessionRepository;
+import org.springframework.session.hazelcast.PrincipalNameExtractor;
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
+import org.springframework.util.SocketUtils;
+
+// tag::class[]
+@EnableHazelcastHttpSession(maxInactiveIntervalInSeconds = 300)
+@Configuration
+public class HazSessionConfig {
+
+	@Bean(destroyMethod = "shutdown")
+	public HazelcastInstance hazelcastInstance() {
+		Config config = new Config();
+
+		int port = SocketUtils.findAvailableTcpPort();
+
+		config.getGroupConfig().setName("qa_HazelcastDistributedSession");
+		 
+//		config.getNetworkConfig()
+//				.setPort(port)
+//				.getJoin().getMulticastConfig().setEnabled(false);
+		
+		config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
+        config.getNetworkConfig().getJoin().getTcpIpConfig().setEnabled(true).addMember("127.0.0.1:"+port);
+        config.setInstanceName("qa_HazelcastDistributedSession");
+        config.getNetworkConfig().setPort(port);
+
+		System.out.println("Hazelcast port #: " + port);
+
+		SerializerConfig serializer = new SerializerConfig()
+				.setImplementation(new ObjectStreamSerializer())
+				.setTypeClass(Object.class);
+
+		config.getSerializationConfig()
+				.addSerializerConfig(serializer);
+
+		MapAttributeConfig attributeConfig = new MapAttributeConfig()
+				.setName(HazelcastSessionRepository.PRINCIPAL_NAME_ATTRIBUTE)
+				.setExtractor(PrincipalNameExtractor.class.getName());
+
+		config.getMapConfig(HazelcastSessionRepository.DEFAULT_SESSION_MAP_NAME)
+				.addMapAttributeConfig(attributeConfig)
+				.addMapIndexConfig(new MapIndexConfig(
+						HazelcastSessionRepository.PRINCIPAL_NAME_ATTRIBUTE, false));
+
+		return Hazelcast.newHazelcastInstance(config);
+	}
+
+}
+// end::class[]
+*/
---
+++ zuulgateway/src/main/java/co/gateway/zuul/config/SecurityConfig.java
@@ -0,0 +1,70 @@
+package co.gateway.zuul.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.client.RestTemplate;
+
+import co.gateway.zuul.filter.CorsFilter;
+
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    	
+        http
+        .csrf().disable()
+       .authorizeRequests()
+                .antMatchers("/")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2Login().loginPage("http://localhost:8089/oauth2/authorization/custom")
+                .successHandler(customAuthenticationSuccessHandler());
+    	http.addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(OAuth2AuthorizedClientService clientService) {
+        RestTemplate restTemplate = new RestTemplate();
+        List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
+        if (CollectionUtils.isEmpty(interceptors)) {
+            interceptors = new ArrayList<>();
+        }
+        interceptors.add(new AuthorizationHeaderInterceptor(clientService));
+        restTemplate.setInterceptors(interceptors);
+        return restTemplate;
+    }
+
+    
+    @Bean
+    public AuthenticationSuccessHandler customAuthenticationSuccessHandler() {
+        return new CustomAuthenticationSuccessHandler();
+    }
+
+    @Bean
+    public OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> accessTokenResponseClient() {
+    	return new DefaultAuthorizationCodeTokenResponseClient();
+    }
+
+    @Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/resources/**", "/static/**", "/javascript/**");
+	}
+}
